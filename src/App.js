bb4ee4e6252da384784f5af8bbd3ade2da8af16c
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-10-29 20:28:09 +0900

    feat: get random lotto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import {Console} from '@woowacourse/mission-utils';
+import {Console, MissionUtils} from '@woowacourse/mission-utils';
 import {inputPurchaseMoney} from "./utils/inputPurchaseMoney.js";
+import Lotto from "./Lotto.js";
+import {getLotto} from "./utils/getLotto.js";
 
 class App {
   async run() {
     const purchaseAmount = await inputPurchaseMoney();
+    Console.print(`\n${purchaseAmount}개를 구매했습니다.`);
+    getLotto(purchaseAmount);
   }
 }
 
---
+++ src/utils/getLotto.js
@@ -0,0 +1,16 @@
+import {Console, MissionUtils} from "@woowacourse/mission-utils";
+import Lotto from "../Lotto.js";
+
+export function getLotto(purchaseAmount) {
+  const lottoArray = [];
+  for (let i = 0; i < purchaseAmount; i += 1) {
+    const randomNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+    const lotto = new Lotto(randomNumbers);
+    lottoArray.push(lotto);
+  }
+
+  lottoArray.forEach((lotto) => {
+    const numbers = lotto.getNumbers();
+    Console.print(`[${numbers.join(', ')}]`);
+  })
+}
