4c3fa64ae5e1048a3cea93549d9231b7aa58a118
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-10-29 20:23:31 +0900

    refactor: fix 10000 to 1000

--- src/utils/inputPurchaseMoney.js
+++ src/utils/inputPurchaseMoney.js
@@ -2,5 +2,5 @@ import {Console} from "@woowacourse/mission-utils";
 
 export async function inputPurchaseMoney() {
   const purchaseMoney = await Console.readLineAsync('로또 구입 금액을 입력해주세요.\n');
-  return purchaseMoney / 10000;
+  return purchaseMoney / 1000;
 }
