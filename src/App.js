3e700486a75db4705df504cf58f59bfe7b00215b
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-10-29 23:07:14 +0900

    feat: add profit function

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import {inputPurchaseMoney} from "./utils/inputPurchaseMoney.js";
 import {getLotto} from "./utils/getLotto.js";
 import {inputPickedBonusNumber, inputPickedLottoNumber} from "./utils/inputWinningNumber.js";
 import {calculateResults} from "./utils/calculateResults.js";
+import {printProfit} from "./utils/printProfit.js";
 
 class App {
   async run() {
-    const purchaseAmount = await inputPurchaseMoney();
+    const purchaseMoney = await inputPurchaseMoney();
+    const purchaseAmount = purchaseMoney / 1000;
 
     Console.print(`\n${purchaseAmount}개를 구매했습니다.\n`);
     const lottoArray = getLotto(purchaseAmount);
@@ -17,6 +19,7 @@ class App {
     Console.print('당첨 통계\n---');
     const rankCounts = calculateResults(lottoArray, pickedLottoNumber, pickedBonusNumber);
     Console.print(`3개 일치 (5,000원) - ${rankCounts["5th"]}개\n4개 일치 (50,000원) - ${rankCounts["4th"]}개\n5개 일치 (1,500,000원) - ${rankCounts["3rd"]}개\n5개 일치, 보너스 볼 일치 (30,000,000원) - ${rankCounts["2nd"]}개\n6개 일치 (2,000,000,000원) - ${rankCounts["1st"]}개\n`)
+    printProfit(purchaseMoney, rankCounts);
   }
 }
 
---
+++ src/utils/printProfit.js
@@ -0,0 +1,7 @@
+import {Console} from "@woowacourse/mission-utils";
+
+export function printProfit(purchaseMoney, rankCounts) {
+  const sum = 5000 * rankCounts["5th"] + 50000 * rankCounts["4th"] + 1500000 * rankCounts["3rd"] + 30000000*rankCounts["2nd"] + 2000000000 * rankCounts["1st"]
+  const profit = sum * 100 / purchaseMoney;
+  Console.print(`총 수익률은 ${profit}%입니다.`);
+}
