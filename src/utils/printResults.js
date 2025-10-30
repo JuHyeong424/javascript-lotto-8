cdbef4b467bbef9685ce142126e35c37e7493d8a
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-10-30 16:37:42 +0900

    fix: access HEADER correctly in PRINT_RESULTS

--- src/utils/printResults.js
+++ src/utils/printResults.js
@@ -1,11 +1,15 @@
 import {Console} from "@woowacourse/mission-utils";
 import {PRINT_RESULTS} from "../constants/printConstants.js";
 
+
+
 export function printResults(rankCounts) {
-  Console.print(PRINT_RESULTS.HEADER);
-  Console.print(PRINT_RESULTS(rankCounts).FIFTH);
-  Console.print(PRINT_RESULTS(rankCounts).FOURTH);
-  Console.print(PRINT_RESULTS(rankCounts).THIRD);
-  Console.print(PRINT_RESULTS(rankCounts).SECOND);
-  Console.print(PRINT_RESULTS(rankCounts).FIRST);
+  const { HEADER, FIFTH, FOURTH, THIRD, SECOND, FIRST } = PRINT_RESULTS(rankCounts);
+
+  Console.print(HEADER);
+  Console.print(FIFTH);
+  Console.print(FOURTH);
+  Console.print(THIRD);
+  Console.print(SECOND);
+  Console.print(FIRST);
 }
