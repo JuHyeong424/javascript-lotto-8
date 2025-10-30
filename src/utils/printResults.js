49b3139ac85ec4d6c858d450b94db14add59be73
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-10-30 16:39:02 +0900

    fix: delete empty enter lint

--- src/utils/printResults.js
+++ src/utils/printResults.js
@@ -1,8 +1,6 @@
 import {Console} from "@woowacourse/mission-utils";
 import {PRINT_RESULTS} from "../constants/printConstants.js";
 
-
-
 export function printResults(rankCounts) {
   const { HEADER, FIFTH, FOURTH, THIRD, SECOND, FIRST } = PRINT_RESULTS(rankCounts);
 
